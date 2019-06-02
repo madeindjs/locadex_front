45039cebe81b6cb29dde9234ee6c015edb2bde6d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-06-02 10:45:03 +0200

    Add a product list element

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app" class="container-fluid">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-6">
+        <ProductList />
+      </div>
+      <div class="col-6">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'app',
   components: {
+    ProductList,
     HelloWorld
   }
 }
---
+++ src/components/ProductList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Liste des produits</h2>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        :class="selectedProductId == product.id ? 'active' : ''"
+        :key="product.id"
+        v-for="product in products"
+        @click="selectedProductId = product.id"
+      >
+        {{ product.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductList',
+  data: () => {
+    return {
+      products: [
+        { id: 1, name: 'Nike AirMax' },
+        { id: 2, name: 'Nike AirMax' },
+      ],
+      selectedProductId: null,
+    }
+  },
+}
+</script>
