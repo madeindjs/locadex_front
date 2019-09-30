2dec8a61db8f74a7a39e4009752f4b4ae8a12845
Blaise Popote <blaisepopote@MacBook-Pro-de-Blaise.local>
2019-09-30 16:54:48 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,89 @@
       <ProductMap class="col-12 col-sm-6 col-md-8 px-0" id="map-container"/>
       <ProductList class="col-12 col-sm-6 col-md-4" id="product-container" />
     </div>
+    <!-- Footer -->
+    <footer class="page-footer font-small bg-light pt-4">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+          <!-- Content -->
+          <h5 class="text-uppercase">Locadex.ch</h5>
+          <p>Find products in local stores</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">My store</a>
+            </li>
+            <li>
+              <a href="#!">Product locator</a>
+            </li>
+            <li>
+              <a href="#!">Help</a>
+            </li>
+            <li>
+              <a href="#!">About us</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">Follow and contact Locadex</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">About us</a>
+            </li>
+            <li>
+              <a href="#!">Contact</a>
+            </li>
+            <li>
+              <a href="#!">Facebook</a>
+            </li>
+            <li>
+              <a href="#!">Youtube</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="https://locadex.ch/"> Locadex.ch</a>
+    </div>
+    <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
   </div>
 </template>
 
